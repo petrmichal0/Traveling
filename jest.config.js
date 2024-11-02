0a8ae3a2176789215a49769036bbc9e7434042b0
Petr Michal <petrmichal0@gmail.com>
2024-11-02 22:10:55 +0100

    change to vite and install all dependencies and updfate to last verisons #1

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
